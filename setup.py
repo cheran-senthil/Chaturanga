e479187c399995247f102971f0b28eeacf402947
Cheran Senthil <cheran.v.senthil@gmail.com>
2018-07-24 19:03:08 +0530

    Updated version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 
 setup(name='Chaturanga',
-      version='0.1.1',
+      version='0.1.2',
       description='Chess API for python',
       long_description=long_description,
       long_description_content_type='text/markdown',
