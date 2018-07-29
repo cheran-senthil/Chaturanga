ad66ed4d2fa2474d289160c456f235d5dd92d886
Cheran Senthil <cheran.v.senthil@gmail.com>
2018-07-29 12:32:02 +0530

    Fix promotion for white

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 
 setup(name='Chaturanga',
-      version='0.1.7',
+      version='0.1.8',
       description='Chess API for python',
       long_description=long_description,
       long_description_content_type='text/markdown',
